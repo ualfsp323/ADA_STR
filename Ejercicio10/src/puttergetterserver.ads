2192383cf3c06dcd000b94b9a4d7a2c2a7b492fb
FSP-1 <f.s.p282002@gmail.com>
2025-05-13 19:47:23 +0200

    Ejercicio 10 opcional terminado

---
+++ Ejercicio10/src/puttergetterserver.ads
@@ -0,0 +1,15 @@
+with Putter;  --  << importa tu paquete de salida
+
+package PutterGetterServer is
+   --  Sincroniza el acceso a la consola
+   procedure Request_Access;
+   procedure Release_Access;
+
+   --  Escribe de forma segura usando Putter
+   procedure Safe_Print (Msg : in String);
+
+   --  Tarea cliente que hace N lecturas/escrituras
+   task type PutterGetter_Client is
+      entry Start (Name : in Character; Iterations : in Positive);
+   end PutterGetter_Client;
+end PutterGetterServer;
